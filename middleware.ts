733ab6f85d3023d3bb33d6a670b457ee515fa423
Jingli-123 <jingli88669@gmail.com>
2026-05-11 00:00:10 +1000

    modify middleware

--- middleware.ts
+++ middleware.ts
@@ -20,23 +20,34 @@
 //     '/(api|trpc)(.*)',
 //   ],
 // };
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher([
-  "/",
-  "/sign-in(.*)",
-  "/sign-up(.*)",
-]);
+// const isPublicRoute = createRouteMatcher([
+//   "/",
+//   "/sign-in(.*)",
+//   "/sign-up(.*)",
+// ]);
 
-export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect();
-  }
-});
+// export default clerkMiddleware(async (auth, req) => {
+//   if (!isPublicRoute(req)) {
+//     await auth.protect();
+//   }
+// });
+
+// export const config = {
+//   matcher: [
+//     "/((?!_next|.*\\..*).*)",
+//     "/(api|trpc)(.*)",
+//   ],
+// };
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
     "/((?!_next|.*\\..*).*)",
     "/(api|trpc)(.*)",
+    "/__clerk/(.*)",
   ],
 };
